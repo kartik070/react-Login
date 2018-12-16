89026791bb8f13095d85ed85e1d913314fef2e29
kartik <kartik070>
2018-12-17 00:14:48 +0530

    second commit

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -13,23 +13,33 @@ export default class Login extends Component{
   render(){
     return(
       <React.Fragment>
-      <div>
-        <form className="login">
+
+      <div className="login">
+        <form>
             <div className="form-group">
-              <label for="exampleInputEmail1">Email address</label>
+              <h1>Login Form</h1>
+              <label htmlFor="exampleInputEmail1">Email address</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
               <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <label htmlFor="exampleInputPassword1">Password</label>
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
             <div className="form-group form-check">
               <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
             </div>
+            <div className="signup">
+              <a href="signup" >signup</a>
+              <a href="forgotpassword" >forgot password</a>
+            </div>
+            <div className="submit">
             <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+            
         </form>
+        
       </div>  
       </React.Fragment>
 
